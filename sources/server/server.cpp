53f11bc16deeb17fe11d3766a051d1406c171ad1
r_comrad main <comrade_1997@mail.ru>
2023-02-15 01:23:13 +0300

    Version 4.0 is working

--- sources/server/server.cpp
+++ sources/server/server.cpp
@@ -39,8 +39,7 @@ serv::Server::Server(data::DBSettings aDBS) : mDBS(aDBS)
     ([]() { return crow::mustache::load("index.html").render(); });
 
     CROW_ROUTE(app, "/test")
-    ([]() { return "test (#0)"; });
-
+    ([]() { return "All fine!"; });
 
     CROW_ROUTE(app, "/favicon.ico")
     (
