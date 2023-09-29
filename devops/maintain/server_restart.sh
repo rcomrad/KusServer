f307211e11128cb630fbcdd62b4ab80cdd80f7e8
rcomrad <comrade_1997@mail.ru>
2023-09-29 12:21:01 +0300

    g

--- devops/maintain/server_restart.sh
+++ devops/maintain/server_restart.sh
@@ -1,13 +1,15 @@
-# sudo apt-get install sshpass
+cd ./scripts
+sudo chmod +x autopush.bat
+./autopush.bat
+cd ../
 
-. /home/sanya/keys/server.sh
-sh -e ./autopush.bat
+cd ../setup/scripts
+sudo chmod +x autopush.bat
+./key.bat
+cd ../../maintain
 
-# echo $user_password
-# echo sshpass -p $user_password ssh -o StrictHostKeyChecking=no $user_name@$ip "echo -e '$user_password\n' | (sudo -S chmod +x ./server/devops/maintain/scripts/remake.sh)"
-
-sshpass -p $user_password ssh -o StrictHostKeyChecking=no $user_name@$ip "echo -e '$user_password\n' | (sudo -S chmod +x ./server/devops/maintain/scripts/remake.sh)"
-sshpass -p $user_password ssh -o StrictHostKeyChecking=no $user_name@$ip "echo -e '$user_password\n' | (cd  ./server/devops/maintain/scripts; sudo -S ./remake.sh)"
+ssh kussystem "echo -e '%user_password%\n' | (sudo -S chmod +x ./server/devops/maintain/scripts/remake.sh)"
+ssh kussystem "echo -e '%user_password%\n' | (cd  ./server/devops/maintain/scripts; sudo -S ./remake.sh)"
 
 echo FINISHED
-
+pause
