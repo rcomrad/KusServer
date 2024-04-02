b3e73be63c759c2110a6d2823bab66d1025e07bc
Ваше Имя <you@example.com>
2024-04-02 23:08:28 +0300

    No repetitions, additional methods, cleaner code

--- sources/server/middleware.hpp
+++ sources/server/middleware.hpp
@@ -14,8 +14,21 @@ struct Middleware
         const UserData* mUser;
     };
 
+    bool redirection_flag = true;
+
     void before_handle(crow::request& req, crow::response& res, context& ctx);
     void after_handle(crow::request& req, crow::response& res, context& ctx);
+
+    void process_comp_question(crow::response& res,
+                               serv::UserData user,
+                               std::string id);
+    void process_comp_user(crow::response& res,
+                           serv::UserData user,
+                           std::string id);
+
+    void emergency_redirection();
+
+    bool redirection_enabled();
 };
 
 } // namespace serv
