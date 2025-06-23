b695457a09f6f557a1b26cf7a7693c4cc0ba8877
Alex <comrade_1997@mail.ru>
2025-06-23 22:35:37 +0300

    turn on vulkan

--- sources/engine/main_module.cpp
+++ sources/engine/main_module.cpp
@@ -1,12 +1,12 @@
 #include "main_module.hpp"
 
-// #include <vulkan/vulkan.h>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
 
 auto module_name = "engine";
 
-REG_MODULE(module_name, engine, MainModule);
+REG_MODULE(module_name, engine::MainModule);
 
 engine::MainModule::MainModule() : core::ThreadModule(module_name)
 {
@@ -17,23 +17,23 @@ engine::MainModule::threadInitialize()
 {
     std::cout << "inside" << std::endl;
 
-    // VkApplicationInfo appInfo = {};
-    // appInfo.sType             = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    // appInfo.pApplicationName  = "Pong";
-    // appInfo.pEngineName       = "Ponggine";
+    VkApplicationInfo appInfo = {};
+    appInfo.sType             = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName  = "Pong";
+    appInfo.pEngineName       = "Ponggine";
 
-    // VkInstanceCreateInfo instanceInfo = {};
-    // instanceInfo.sType                = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    // instanceInfo.pApplicationInfo     = &appInfo;
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType                = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo     = &appInfo;
 
-    // VkInstance instance;
+    VkInstance instance;
 
-    // VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
-    // if (result == VK_SUCCESS)
+    VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
+    if (result == VK_SUCCESS)
     {
         std::cout << "Successfully created vulkan instance" << std::endl;
     }
-    // else
+    else
     {
         std::cout << "ERROR" << std::endl;
     }
